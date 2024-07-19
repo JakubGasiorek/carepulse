8066134eb0a2325ba9e49d890664fc6a96e4a633
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-07-19 16:12:34 +0200

    Added getAppointment action and modified Success page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,4 +1,4 @@
-'use server'
+"use server";
 
 import { ID } from "node-appwrite";
 import {
@@ -20,6 +20,20 @@ export const createAppointment = async (
     );
 
     return parseStringify(newAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getAppointment = async (appoitmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appoitmentId
+    );
+
+    return parseStringify(appointment);
   } catch (error) {
     console.log(error);
   }
