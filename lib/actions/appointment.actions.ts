da8b14eabe12ebdaedde2815ce91e7dabc177a3c
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-07-20 19:07:31 +0200

    Added updateAppointment  action

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -8,6 +8,7 @@ import {
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
+import { revalidatePath } from "next/cache";
 
 export const createAppointment = async (
   appointment: CreateAppointmentParams
@@ -74,6 +75,31 @@ export const getRecentAppointmentList = async () => {
       documents: appointments.documents,
     };
     return parseStringify(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateAppointment = async ({
+  appointmentId,
+  userId,
+  appointment,
+  type,
+}: UpdateAppointmentParams) => {
+  try {
+    const updateAppointment = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
+
+    if (!updateAppointment) {
+      throw new Error("Appointment not found");
+    }
+
+    revalidatePath("/admin");
+    return parseStringify(updateAppointment);
   } catch (error) {
     console.log(error);
   }
